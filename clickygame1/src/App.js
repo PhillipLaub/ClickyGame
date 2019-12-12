40028832b40665f15573f485213a426174eab41a
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-12-12 11:11:54 -0800

    Removing index.js from components, not necessary per discussion with TA

--- clickygame1/src/App.js
+++ clickygame1/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import Card from './components/Card';
-import Header from './components/Header';
-import Wrapper from './components/Wrapper';
-import cards from './cards.json';
-
+import React from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
 class App extends Component {
   //this.state.cards set to access json array
   state = {
-    cards : cards,
-    score : 0,
-    highscore : 0
+    cards: cards,
+    score: 0,
+    highscore: 0
   };
 
   //Reset Game, function is triggered when same card is click more than once
-  resetGame = () =>{
-
+  resetGame = () => {
     //check to see if score is greater than highscore
     if (this.state.score > this.state.highscore) {
       //if score is greater, set highscore equal to score
-      this.setState({highscore: this.state.score}, function() {
+      this.setState({ highscore: this.state.score }, function() {
         //console.log highscore
-        console.log("Highscore: ")
+        console.log("Highscore: ");
         console.log(this.state.highscore);
       });
     }
@@ -35,35 +33,33 @@ class App extends Component {
     alert(`Game Over! \nSCORE: ${this.state.score}/12`);
 
     //reset score to zero
-    this.setState({score: 0})
+    this.setState({ score: 0 });
     return true;
-  }
+  };
 
   //method to check if card has been previously clicked
   clickCounter = id => {
-    //pass in object and index for access 
+    //pass in object and index for access
     this.state.cards.find((obj, index) => {
-      if(obj.id === id) {
-
+      if (obj.id === id) {
         //if the card click count is equal to zero...
-        if(cards[index].count === 0) {
-          
+        if (cards[index].count === 0) {
           //increase the card click count by one
           cards[index].count = cards[index].count + 1;
 
           //increase the score by one
-          this.setState({score : this.state.score + 1}, function () {
+          this.setState({ score: this.state.score + 1 }, function() {
             //console log updated score value
             console.log(this.state.score);
-          })
+          });
 
           //randomize locations for each card
-          this.state.cards.sort(() => Math.random() - .5)
+          this.state.cards.sort(() => Math.random() - 0.5);
           return true;
         }
 
         //check to see if user reached score of 12, if so alert win, and trigger reset
-        if (this.state.score === 12){
+        if (this.state.score === 12) {
           alert(`You Win! Great Job! \nSCORE: ${this.state.score}/12`);
           this.resetGame();
         }
@@ -74,20 +70,28 @@ class App extends Component {
         }
       }
     });
-
-  }
-
-
+  };
 
   render() {
+    //render components and pass properties
     return (
-        <Wrapper>
-
-        <Header />
-
-        <Card />
-
-        </Wrapper>
+      <Wrapper>
+        {/* Passing properties score and highscore into the Header Component*/}
+        <Header score={this.state.score} highscore={this.state.highscore}>
+          ClickyGame
+        </Header>
+        {/* Loop/map through cards json, create 'card' for each, with properties, clickcount, id, key, and image */}
+        {this.state.cards.map(card => (
+          <Card
+            //Pass clickCount property
+            clickCount={this.clickCount}
+            // id and image already defined in the json object, attaching key property to not trigger error in React
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
