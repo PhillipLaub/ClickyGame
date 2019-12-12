24948a4ca58ccccc2a4601edc6ac06f7fa6ba98e
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-12-11 17:32:30 -0800

    basic code setup for components and App.js

--- clickygame1/src/App.js
+++ clickygame1/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import cards from './cards.json';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends Component {
+  //this.state.cards set to access json array
+  state = {
+    cards : cards,
+    score : 0,
+    highscore : 0
+  };
+
+
+  render() {
+    return (
+        <Wrapper>
+
+        <Header />
+
+        <Card />
+
+        </Wrapper>
+    );
+  }
 }
 
 export default App;
