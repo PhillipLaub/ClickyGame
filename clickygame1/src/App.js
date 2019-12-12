461f54dca3e19a6124c1416d0097cac8359712d0
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-12-12 10:44:21 -0800

    writing reset game functionality

--- clickygame1/src/App.js
+++ clickygame1/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import cards from './cards.json';
 
+//create gamme logic that if score = 12 user wins and game resets
+
 class App extends Component {
   //this.state.cards set to access json array
   state = {
@@ -11,6 +13,25 @@ class App extends Component {
     score : 0,
     highscore : 0
   };
+
+  //Reset Game, function is triggered when same card is click more than once
+  resetGame = () =>{
+    //check to see if score is greater than highscore
+    if (this.state.score > this.state.highscore) {
+      //if score is greater, set highscore equal to score
+      this.setState({highscore: this.state.score}, function() {
+        //console.log highscore
+        console.log(this.state.highscore);
+      });
+    }
+
+    //set card click count back to zero for all cards
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    //alert user game over
+    alert("Game Over")
+  }
 
 
 
